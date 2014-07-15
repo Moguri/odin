aafa627f13db179c25a834094fc20609faf90a3c
Mitchell Stokes <mogurijin@gmail.com>
2014-07-14 18:55:52 -0700

    Docs: Adding plantuml to the repo to help ReadTheDocs.

--- docs/conf.py
+++ docs/conf.py
@@ -257,3 +257,7 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+
+# -- PlantUML -------------------------------------------------------------
+
+plantuml = 'java -jar ' + os.path.abspath('./plantuml.jar')
